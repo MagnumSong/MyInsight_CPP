dfc4adaf88d8fcc4ff420b6b0427ac5ddd02ec9b
MagnumSong <983174628@qq.com>
2018-01-31 17:56:08 +0800

    肯爱千金轻一笑

--- Harbin/Harbin/main.cpp
+++ Harbin/Harbin/main.cpp
@@ -1,11 +1,50 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#pragma mark 快速排序
+using namespace std;
+
+void insert_sort(int a[],int len)
+{
+    int i=0,j=0,temp=0;
+    for(i=1;i<len;i++)
+    {
+        temp =a[i];
+        for(j=i-1;j>=0&&temp<a[j];j--)
+        {
+            a[j+1]=a[j];
+        }
+        a[j+1]=temp;
+    }
+}
+
+
+void print_array(int a[], int len)
+{
+    for(int i=0;i<len;i++)
+    {
+        //cout<<a[i]<<;
+        //std::cout<<a[i]<<"";
+    }
+    //std::cout<<std::endl;
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+
+    int a1[] = {1,3,5,9,8,6,4,2,7};
+    //std::cout<<"before sort:";
+    print_array(a1,9);
+    insert_sort(a1,9);
+    //std::cout<<"after sort:";
+    print_array(a1,9);
 
     // 进程
     return a.exec();
--- Harbin/Harbin/mainwindow.cpp
+++ Harbin/Harbin/mainwindow.cpp
@@ -1,35 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-using namespace std;
+// using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //BubbleSort();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-// 直接插入排序
-void insert_sort(int a[],int len)
-{
-    int i=0,j=0,temp=0;
-    for(i=1;i<len;i++)
-    {
-        temp =a[i];
-        for(j=i-1;j>=0&&temp<a[j];j--)
-        {
-            a[j+1]=a[j];
-        }
-        a[j+1]=temp;
-    }
-}
-
 
 
