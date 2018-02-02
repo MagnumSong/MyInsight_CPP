e850611fdaea97695b5cba92c950a5c30cbc43a9
MagnumSong <983174628@qq.com>
2018-02-02 17:44:59 +0800

    似曾相识燕归来

--- Harbin/Harbin/main.cpp
+++ Harbin/Harbin/main.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDialog>
 #include <QLabel>
 
 #pragma mark 快速排序
@@ -8,11 +9,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-    QLabel *label = new QLabel(&w);
-    label->setText("滚滚长江东逝水");
-    label->resize(200, 20);
-    label->move(120, 120);
 
     w.show();
 
