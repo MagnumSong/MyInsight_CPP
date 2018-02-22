7ef32581d89dde777c502f31eb9f8b69524fa6d0
MagnumSong <983174628@qq.com>
2018-02-22 10:46:58 +0800

    独在异乡为异客

---
+++ PingDingShan/PingDingShan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PingDingShan
+//
+//  Created by SongMenglong on 2018/1/26.
+//  Copyright © 2018年 SongMenglong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ PingDingShan/PingDingShan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PingDingShan
+//
+//  Created by SongMenglong on 2018/1/26.
+//  Copyright © 2018年 SongMenglong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
