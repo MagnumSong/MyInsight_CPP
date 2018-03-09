06b9c4d8151c09faa34488f7d984050157a9e348
MagnumSong <983174628@qq.com>
2018-03-09 09:38:29 +0800

    初为霓裳后六么

--- CPPDemo/CPPDemo/main.cpp
+++ CPPDemo/CPPDemo/main.cpp
@@ -7,18 +7,39 @@
 //
 
 #include <iostream>
+#include <cmath>
+// using编译指令
+using namespace std;
 
+void springOut(int n);
 
 int main(int argc, const char * argv[]) {
-    // using编译指令
-    using namespace std;
     
     
-    cout << "Hello World";
-    cout << "Hello World/n";
+    
+    
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    
+    int carrots;
+    carrots = 25;
+    cout << "Hello " << carrots << " World" << endl;
+    
+    cout << pow(5.0, 8.0) << endl;
+    
+    springOut(9);
     
     return 0;
 }
+
+
+// 定义函数
+void springOut(int n)
+{
+    cout << n << endl;
+}
+
+
 
 /*
  CPPdemo
